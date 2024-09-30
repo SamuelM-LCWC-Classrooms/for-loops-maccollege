094634868ad443f64981ec7502e04b2a1d555dea
maccollege <30079306@lcwc.ac.uk>
2024-09-30 18:06:20 +0000

    Finished.

--- task.py
+++ task.py
@@ -1,20 +1,21 @@
 from random import randrange
 
 def task_1(): # Lottery ticket generator
-
     ticket = []
-
-    # Code here
-    
+    for i in range(6):
+        num = randrange(1, 50)
+        ticket.append(num)
     return ticket
 
 def task_2(): # Countdown
-
+    num = int(input("Enter a number less than 100."))
     output = []
-
-    # Code here
-
+    for i in range(100, num - 1, -1):
+        num2 = i
+        output.append(num2)
     return output
+
+    
 
 def task_3():
     people_cars = {
@@ -27,7 +28,15 @@ def task_3():
     }
 
     car_make_lengths = ()
-
+    for i in people_cars.values():
+        wordlength = len(i)
+        place = list(car_make_lengths)
+        place.append(wordlength)
+        car_make_lengths = tuple(place)
+        
+        
     # Code here
+    print("There will be 3 different sizes of key rings.")
+    return car_make_lengths
 
-    return # Code the sentence here
+print(task_3())
